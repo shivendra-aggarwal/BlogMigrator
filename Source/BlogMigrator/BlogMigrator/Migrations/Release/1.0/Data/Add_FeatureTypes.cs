cafe9e4f30072552d5d47300eb403a8d96971ab5
Shivendra Aggarwal <shivendra.aggarwal@gmail.com>
2017-02-27 22:58:13 +0530

    added missing code files

---
+++ Source/BlogMigrator/BlogMigrator/Migrations/Release/1.0/Data/Add_FeatureTypes.cs
@@ -0,0 +1,24 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogMigrator.Migrations.Release._1._0.Data
+{
+    [Migration(022720171029)]
+    public class Add_FeatureTypes : Migration
+    {
+        public override void Down()
+        {
+            string query = "";
+            Execute.Sql(query);
+        }
+
+        public override void Up()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Source/BlogMigrator/BlogMigrator/Migrations/Release/1.0/Table/Alter_Table_FeatureType_Column_Name.cs
@@ -0,0 +1,32 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogMigrator.Migrations.Release._1._0.Table
+{
+    [Migration(022720171033)]
+    public class Alter_Table_FeatureType_Column_Name : Migration
+    {
+        public override void Down()
+        {
+            string sql = @"IF EXISTS (SELECT * FROM SYS.columns WHERE NAME=N'FeatureTypeName' AND object_id = OBJECT_ID('Feature.FeatureType'))
+                            BEGIN
+                                ALTER TABLE Feature.FeatureType RENAME COLUMN FeatureTypeName TO FeatureName
+                            END
+                            ";
+            Execute.Sql(sql);
+        }
+
+        public override void Up()
+        {
+            string sql = @"IF EXISTS (SELECT * FROM SYS.columns WHERE NAME=N'FeatureName' AND object_id = OBJECT_ID('Feature.FeatureType'))
+                            BEGIN
+                                ALTER TABLE Feature.FeatureType RENAME COLUMN FeatureName TO FeatureTypeName
+                            END";
+            Execute.Sql(sql);
+        }
+    }
+}
