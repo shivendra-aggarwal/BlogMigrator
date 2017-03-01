c13ef4a44ed7bbd775438a384e7a7f5315a4d12c
Shivendra Aggarwal <shivendra.aggarwal@gmail.com>
2017-03-01 22:08:04 +0530

    removed TEXTIMAGE_ON

--- Source/BlogMigrator/BlogMigrator/Migrations/Release/1.0/Table/Add_Menu_Table.cs
+++ Source/BlogMigrator/BlogMigrator/Migrations/Release/1.0/Table/Add_Menu_Table.cs
@@ -45,7 +45,7 @@ namespace BlogMigrator.Migrations.Release._1._0.Table
                                 (
                                 	[MenuId] ASC
                                 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-                                ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+                                ) ON [PRIMARY] 
                             END
 
                                 GO
