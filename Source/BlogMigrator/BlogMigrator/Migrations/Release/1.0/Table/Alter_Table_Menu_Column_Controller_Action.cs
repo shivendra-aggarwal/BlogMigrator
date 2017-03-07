21b31023bbe4c5dd390b5e99fcfe9e1a715dc95a
Shivendra Aggarwal <shivendra.aggarwal@gmail.com>
2017-03-07 23:07:27 +0530

    Added new columns for Menu table

---
+++ Source/BlogMigrator/BlogMigrator/Migrations/Release/1.0/Table/Alter_Table_Menu_Column_Controller_Action.cs
@@ -0,0 +1,42 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogMigrator.Migrations.Release._1._0.Table
+{
+    [Migration(030720171059)]
+    public class Alter_Table_Menu_Column_Controller_Action : Migration
+    {
+        public override void Down()
+        {
+            string sql = @"IF EXISTS (SELECT * FROM SYS.columns WHERE NAME=N'ControllerName' AND object_id = OBJECT_ID('Menu'))
+                            BEGIN
+								ALTER TABLE Menu DROP COLUMN ControllerName
+                            END
+
+                            IF EXISTS (SELECT * FROM SYS.columns WHERE NAME=N'ActionName' AND object_id = OBJECT_ID('Menu'))
+                            BEGIN
+								ALTER TABLE Menu DROP COLUMN ActionName
+                            END
+                            ";
+            Execute.Sql(sql);
+        }
+
+        public override void Up()
+        {
+            string sql = @"IF NOT EXISTS (SELECT * FROM SYS.columns WHERE NAME=N'ControllerName' AND object_id = OBJECT_ID('Menu'))
+                            BEGIN
+								ALTER TABLE Menu ADD COLUMN ControllerName NVARCHAR(100)
+                            END
+
+                            IF NOT EXISTS (SELECT * FROM SYS.columns WHERE NAME=N'ActionName' AND object_id = OBJECT_ID('Menu'))
+                            BEGIN
+								ALTER TABLE Menu ADD COLUMN ActionName NVARCHAR(100)
+                            END";
+            Execute.Sql(sql);
+        }
+    }
+}
---
+++ Source/BlogMigrator/BlogMigrator/Utilities/MigrationVersion.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogMigrator.Utilities
+{
+    public class MigrationVersion : MigrationAttribute
+    {
+        public MigrationVersion(long version) : base(version)
+        {
+
+        }
+
+        
+    }
+}
