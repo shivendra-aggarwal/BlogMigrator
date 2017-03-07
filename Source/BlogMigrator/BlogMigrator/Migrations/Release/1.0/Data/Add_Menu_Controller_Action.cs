82f6bbfd948bc42aef6251cf19a7b2184444dbbb
Shivendra Aggarwal <shivendra.aggarwal@gmail.com>
2017-03-07 23:25:51 +0530

    Added menu master data

---
+++ Source/BlogMigrator/BlogMigrator/Migrations/Release/1.0/Data/Add_Menu_Controller_Action.cs
@@ -0,0 +1,43 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogMigrator.Migrations.Release._1._0.Data
+{
+    [Migration(030720171111)]
+    public class Add_Menu_Controller_Action : Migration
+    {
+        public override void Up()
+        {
+            string sql = @" DELETE FROM Menu;
+                            DECLARE @ParentId int
+                    
+                            INSERT Menu(MenuName,MenuTitle, ParentID,MenuOrder, ControllerName,ActionName)
+                            VALUES('Dashboard', 'DashBoard',NULL,1,'Dashboard','Index');
+                        
+                            INSERT Menu(MenuName,MenuTitle, ParentID,MenuOrder, ControllerName,ActionName)
+                            VALUES('FeatureManagement', 'Feature Management',NULL,2,NULL,NULL);
+
+                            Select @ParentId = Identifier FROM Menu WHERE MenuName='FeatureManagement';
+                            INSERT Menu(MenuName,MenuTitle, ParentID,MenuOrder, ControllerName,ActionName)
+                            VALUES('Features', 'Features',@ParentId,1,'Features','Index');
+
+                            INSERT Menu(MenuName,MenuTitle, ParentID,MenuOrder, ControllerName,ActionName)
+                            VALUES('Tasks', 'Tasks',@ParentId,2,'Tasks','Index');
+                           ";
+
+            Execute.Sql(sql);
+        }
+
+        public override void Down()
+        {
+            string sql = "DELETE FROM Menu;";
+
+            Execute.Sql(sql);
+        }
+
+    }
+}
