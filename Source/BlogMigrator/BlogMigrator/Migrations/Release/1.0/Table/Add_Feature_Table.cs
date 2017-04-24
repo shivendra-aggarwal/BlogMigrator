c462ab9a4c305a755ec80bbd231aaba65e03f09b
Shivendra Aggarwal <shivendra.aggarwal@gmail.com>
2017-04-24 23:53:41 +0530

    removed go as its not working inside begin and end

--- Source/BlogMigrator/BlogMigrator/Migrations/Release/1.0/Table/Add_Feature_Table.cs
+++ Source/BlogMigrator/BlogMigrator/Migrations/Release/1.0/Table/Add_Feature_Table.cs
@@ -19,8 +19,6 @@ namespace BlogMigrator.Migrations.Release._1._0.Table
                                     ALTER TABLE [dbo.Feature] DROP CONSTRAINT [FK_Feature_FeatureType]
                                 END
                                 
-                                GO
-
                                 DROP TABLE [Feature]
                             END";
             Execute.Sql(sql);
@@ -52,14 +50,11 @@ namespace BlogMigrator.Migrations.Release._1._0.Table
                                 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
                                 ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
                                 
-                                GO
-                                
                                 ALTER TABLE [Feature]  WITH CHECK ADD  CONSTRAINT [FK_Feature_FeatureType] FOREIGN KEY([FeatureTypeId])
                                 REFERENCES [FeatureType] ([Identifier])
-                                GO
                                 
                                 ALTER TABLE [Feature] CHECK CONSTRAINT [FK_Feature_FeatureType]
-                                GO
+                                
                             END";
 
             Execute.Sql(sql);
