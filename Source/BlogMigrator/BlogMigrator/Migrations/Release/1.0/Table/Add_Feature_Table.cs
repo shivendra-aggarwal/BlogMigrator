a9a704a6e53231fc1a0b796422034383d90f652a
Shivendra Aggarwal <shivendra.aggarwal@gmail.com>
2017-04-24 23:48:06 +0530

    Added migrations related to featuretype and feature

---
+++ Source/BlogMigrator/BlogMigrator/Migrations/Release/1.0/Table/Add_Feature_Table.cs
@@ -0,0 +1,67 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogMigrator.Migrations.Release._1._0.Table
+{
+    [Migration(240420171145)]
+    public class Add_Feature_Table : Migration
+    {
+        public override void Down()
+        {
+            string sql = @"IF EXISTS (SELECT * FROM SYS.objects WHERE NAME=N'Feature' AND type='U')
+                            BEGIN
+                                IF EXISTS (SELECT * FROM sys.foreign_keys WHERE object_id = OBJECT_ID(N'dbo.FK_Feature_FeatureType') AND parent_object_id = OBJECT_ID(N'dbo.Feature'))
+                                BEGIN
+                                    ALTER TABLE [dbo.Feature] DROP CONSTRAINT [FK_Feature_FeatureType]
+                                END
+                                
+                                GO
+
+                                DROP TABLE [Feature]
+                            END";
+            Execute.Sql(sql);
+        }
+
+        public override void Up()
+        {
+            string sql = @"IF NOT EXISTS (SELECT * FROM SYS.objects WHERE NAME=N'Feature' AND type='U')
+                            BEGIN
+                                CREATE TABLE [Feature](
+                                	[Identifier] [int] IDENTITY(1,1) NOT NULL,
+                                	[FeatureName] [nvarchar](max) NULL,
+                                	[FeatureDescription] [nvarchar](max) NULL,
+                                	[IsCompleted] [bit] NOT NULL,
+                                	[CompletedOn] [datetime] NULL,
+                                	[StartedOn] [datetime] NULL,
+                                	[IsActive] [bit] NOT NULL,
+                                	[DocUrl] [nvarchar](max) NULL,
+                                	[ImageUrl] [nvarchar](max) NULL,
+                                	[FeatureTypeId] [int] NOT NULL,
+                                	[ProjectId] [int] NOT NULL,
+                                	[CreatedDate] [datetime] NOT NULL,
+                                	[CreatedBy] [nvarchar](max) NULL,
+                                	[UpdatedDate] [datetime] NOT NULL,
+                                	[UpdatedBy] [nvarchar](max) NULL,
+                                 CONSTRAINT [PK_Feature] PRIMARY KEY CLUSTERED 
+                                (
+                                	[Identifier] ASC
+                                )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+                                ) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+                                
+                                GO
+                                
+                                ALTER TABLE [Feature]  WITH CHECK ADD  CONSTRAINT [FK_Feature_FeatureType] FOREIGN KEY([FeatureTypeId])
+                                REFERENCES [FeatureType] ([Identifier])
+                                GO
+                                
+                                ALTER TABLE [Feature] CHECK CONSTRAINT [FK_Feature_FeatureType]
+                                GO";
+
+            Execute.Sql(sql);
+        }
+    }
+}
---
+++ Source/BlogMigrator/BlogMigrator/Migrations/Release/1.0/Table/Add_Table_FeatureType.cs
@@ -0,0 +1,41 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogMigrator.Migrations.Release._1._0.Table
+{
+    [Migration(240420171142)]
+    public class Add_Table_FeatureType : Migration
+    {
+        public override void Down()
+        {
+            string sql = @"IF EXISTS (SELECT * FROM SYS.objects WHERE NAME=N'FeatureType' AND type='U')
+                            BEGIN
+                                DROP TABLE [FeatureType]
+                            END";
+            Execute.Sql(sql);
+        }
+
+        public override void Up()
+        {
+            string sql = @"IF NOT EXISTS (SELECT * FROM SYS.objects WHERE NAME=N'FeatureType' AND type='U')
+                            BEGIN
+                                CREATE TABLE [FeatureType](
+                                	[Identifier] [int] IDENTITY(1,1) NOT NULL,
+                                	[FeatureTypeName] [nvarchar](200) NOT NULL,
+                                 CONSTRAINT [PK_FeatureType] PRIMARY KEY CLUSTERED 
+                                (
+                                	[Identifier] ASC
+                                )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+                                ) ON [PRIMARY]
+                                
+                                GO
+                                ";
+
+            Execute.Sql(sql);
+        }
+    }
+}
