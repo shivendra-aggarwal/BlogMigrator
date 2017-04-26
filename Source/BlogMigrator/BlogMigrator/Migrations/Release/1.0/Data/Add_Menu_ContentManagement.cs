9cb6dda2cd64eba92877df841d185ed3c6ed72d0
Shivendra Aggarwal <shivendra.aggarwal@gmail.com>
2017-04-26 23:30:06 +0530

    Added menus related to test data configuration

---
+++ Source/BlogMigrator/BlogMigrator/Migrations/Release/1.0/Data/Add_Menu_ContentManagement.cs
@@ -0,0 +1,38 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogMigrator.Migrations.Release._1._0.Data
+{
+    [Migration(042620171125)]
+    public class Add_Menu_ContentManagement : Migration
+    {
+        public override void Up()
+        {
+            string sql = @" DECLARE @ParentId int
+                    
+                            INSERT Menu(MenuName,MenuTitle, ParentID,MenuOrder, ControllerName,ActionName)
+                            VALUES('ContentManagement', 'Content Management',NULL,99,'NULL','NULL');
+                        
+                            Select @ParentId = Identifier FROM Menu WHERE MenuName='ContentManagement';
+                            INSERT Menu(MenuName,MenuTitle, ParentID,MenuOrder, ControllerName,ActionName)
+                            VALUES('SetupTestData', 'Setup Test Data',@ParentId,1,'TestData','GetModules');
+
+                           ";
+
+            Execute.Sql(sql);
+        }
+
+        public override void Down()
+        {
+            string sql = @"DELETE FROM Menu WHERE MenuName='SetupTestData';
+                           DELETE FROM Menu WHERE MenuName='ContentManagement';";
+
+            Execute.Sql(sql);
+        }
+
+    }
+}
